3a4e547369b4e8af9a4807cda06b5e11a771cf80
flashmob1441 <alexahdro33@gmail.com>
2025-02-02 11:15:48 +0500

    Init.

---
+++ src/main/java/org/flashmob/hunterXHunterPlugin/HunterXHunterPlugin.java
@@ -0,0 +1,17 @@
+package org.flashmob.hunterXHunterPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HunterXHunterPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
